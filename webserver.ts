147983fab96ea1cef5dded4b405b7e3690a6d6a1
Elan Ruusamäe <glen@delfi.ee>
2022-03-09 16:36:24 +0200

    Add POST /reload handler

--- webserver.ts
+++ webserver.ts
@@ -1,13 +1,21 @@
 import { serve } from "https://deno.land/std@0.128.0/http/server.ts";
+import { Status, STATUS_TEXT } from "https://deno.land/std@0.128.0/http/http_status.ts";
 
 const hostname = Deno.env.get("SERVER_ADDRESS") || "127.0.0.1";
 const port = Number(Deno.env.get("SERVER_PORT")) || 6781;
 
-const handler = (request: Request): Response => {
-  let body = "Your user-agent is:\n\n";
-  body += request.headers.get("user-agent") || "Unknown";
+const reloadHandler = async (request: Request): Promise<Response> => {
+  return new Response("Restarted", { status: Status.Accepted });
+};
 
-  return new Response(body, { status: 200 });
+const handler = async (request: Request): Promise<Response> => {
+  const url = new URL(request.url);
+
+  if (request.method === "POST" && url.pathname === "/reload") {
+    return reloadHandler(request);
+  }
+
+  return new Response(STATUS_TEXT.get(Status.NotFound), { status: Status.NotFound });
 };
 
 console.log(`HTTP webserver running. Access it at: http://${hostname}:${port}`);
