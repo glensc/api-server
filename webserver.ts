62ccec0ea675972c36e0740376934ec2a552fa4b
Elan Ruusamäe <glen@delfi.ee>
2022-03-09 16:24:53 +0200

    Add address/port configuration from env

--- webserver.ts
+++ webserver.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.128.0/http/server.ts";
 
-const port = 8080;
+const hostname = Deno.env.get("SERVER_ADDRESS") || "127.0.0.1";
+const port = Number(Deno.env.get("SERVER_PORT")) || 6781;
 
 const handler = (request: Request): Response => {
   let body = "Your user-agent is:\n\n";
@@ -9,5 +10,5 @@ const handler = (request: Request): Response => {
   return new Response(body, { status: 200 });
 };
 
-console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
-await serve(handler, { port });
+console.log(`HTTP webserver running. Access it at: http://${hostname}:${port}`);
+await serve(handler, { hostname, port });
