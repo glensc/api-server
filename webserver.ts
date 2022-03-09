54d90b74befc6a478b850c991325e7171ee785b7
Elan Ruusamäe <glen@delfi.ee>
2022-03-09 17:49:07 +0200

    Implement command execution
    https://medium.com/deno-the-complete-reference/run-shell-commands-in-deno-26c3e9b72e03

--- webserver.ts
+++ webserver.ts
@@ -5,13 +5,50 @@ const hostname = Deno.env.get("SERVER_ADDRESS") || "127.0.0.1";
 const port = Number(Deno.env.get("SERVER_PORT")) || 6781;
 const serverToken = Deno.env.get("SERVER_TOKEN") || Math.random();
 
+const runCommand = async (cmd: string[]) => {
+  const p = Deno.run({
+    stdin: "null",
+    stdout: "piped",
+    stderr: "piped",
+    cmd,
+  });
+
+  type AwaitType = [Deno.ProcessStatus, Uint8Array, Uint8Array];
+  const [status, stdoutBytes, stderrBytes]: AwaitType = await Promise.all([
+    p.status(),
+    p.output(),
+    p.stderrOutput(),
+  ]);
+  p.close();
+
+  const [stdout, stderr] = [
+    new TextDecoder().decode(stdoutBytes),
+    new TextDecoder().decode(stderrBytes),
+  ];
+  if (stderr) {
+    throw new Error("Error: " + stderr);
+  }
+  if (!status.success) {
+    throw new Error("Error: " + status);
+  }
+
+  return stdout;
+};
+
 const reloadHandler = async (request: Request): Promise<Response> => {
   const authToken = request.headers.get("x-authorization");
   if (authToken !== serverToken) {
     return new Response(STATUS_TEXT.get(Status.Forbidden), { status: Status.Forbidden });
   }
 
-  return new Response("Restarted", { status: Status.Accepted });
+  let output;
+  try {
+    output = await runCommand("echo abcd".split(' '));
+  } catch (e) {
+    return new Response(e.message, { status: Status.ServiceUnavailable });
+  }
+
+  return new Response(output, { status: Status.Accepted });
 };
 
 const handler = async (request: Request): Promise<Response> => {
